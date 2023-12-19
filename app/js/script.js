7a8dee1138c0a1253f3d40abb7ef42439fcf7b6f
Brian-Web-Developer <76440180+Brian-Web-Developer@users.noreply.github.com>
2023-12-19 22:09:13 +0300

    Added a JS Switch button
    Added a JS Switch button

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+
+function change() {
+let background = document.querySelector('#page-pineaple');
+
+     background.classList.remove("orange-session");
+     background.classList.add("green-session");
+
+ 
+
+
+}
+
